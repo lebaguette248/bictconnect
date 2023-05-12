4ec2d6886be2b7ffb1fa135b5cce277064fe6bf9
lebaguette248 <57328849+lebaguette248@users.noreply.github.com>
2023-05-12 10:42:27 +0200

    build: add GET and post function for messages

--- src/Controller/MessageController.php
+++ src/Controller/MessageController.php
@@ -3,25 +3,28 @@
 namespace App\Controller;
 
 use App\DTO\CreateUpdateMessage;
-use App\DTO\FilterUser;
+use App\DTO\Mapper\ShowMessageMapper;
 use App\Entity\Message;
 use App\Repository\MessageRepository;
 use App\Repository\UserRepository;
 
 use FOS\RestBundle\Controller\AbstractFOSRestController;
-use FOS\RestBundle\Controller\Annotations\Post;
+use FOS\RestBundle\Controller\Annotations as Rest;
 use JMS\Serializer\SerializerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 
 #[Route("/api", name: "api_")]
 class MessageController extends AbstractFOSRestController
 {
-    public function __construct(private SerializerInterface $serializer,        //constructor
+    public function __construct(private SerializerInterface $serializer,
+                                private ValidatorInterface $validator,       //constructor
                                 private MessageRepository $repository,
-                                private UserRepository $userRepository){}
+                                private UserRepository $userRepository,
+                                private ShowMessageMapper $mapper){}
 
 
 //    #[Post('/message', name: 'app_message')]
@@ -38,17 +41,35 @@ class MessageController extends AbstractFOSRestController
 //        return $this->json("Post Funktioniert");
 //    }
 
-    #[Rest\Get('/message', name: 'app_users')]
+    #[Rest\Post("/message", "app_message_create")]
+    public function create(Request $request) {
+        $dto = $this->serializer->deserialize($request->getContent(), CreateUpdateMessage::class, "json");  //handles DTO Deserialization
+
+        $errors = $this->validator->validate($dto);
+        
+        if($errors->count() > 0){
+            return $this->json("Heck.", status: 400);
+        }
+        $entity = new Message();
+        $entity->setTitle($dto->title);
+        $entity->setContent($dto->content);
+
+        $user = $this->userRepository->find($dto->id_user);
+        $entity->setUser($user);
+
+        $this->repository->save($entity, true);
+
+        $messageJson = $this->serializer->serialize($this->mapper->mapEntityToDTO($entity), "json");
+
+        return (new JsonResponse())->setContent($messageJson);
+    }
+
+    #[Rest\Get('/message', name: 'app_messages')]
     public function index(Request $request): JsonResponse           //Get function
     {
-        global $con = new \PDO(Datenbank scheiss)
+        $messages = $this->repository->findAll();
 
-        $sql->con = "SELECT * FROM messages WHERE username = :username";
-        $sql->bindParam(':username', $username);
-        $sql->execute();
-        $data = $sql->fetchAll();
-
-        return
+        return $this->json($this->mapper->mapEntitiesToDTOs($messages));
     }
 
 
